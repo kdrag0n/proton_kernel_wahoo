746272677ca366db05e61ca9d6ab2816721db088
Danny Lin <danny@kdrag0n.dev>
2020-05-13 23:48:28 -0700

    flasher: Skip ramdisk unpacking and repacking
    We don't currently modify anything in the ramdisk, so skip these steps
    to speed up the flashing process.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- flasher/tools/ak3-core.sh
+++ flasher/tools/ak3-core.sh
@@ -182,7 +182,7 @@ unpack_ramdisk() {
 ### dump_boot (dump and split image, then extract ramdisk)
 dump_boot() {
   split_boot;
-  unpack_ramdisk;
+  #unpack_ramdisk;
 }
 ###
 
@@ -438,7 +438,7 @@ flash_dtbo() {
 }
 ### write_boot (repack ramdisk then build, sign and write image and dtbo)
 write_boot() {
-  repack_ramdisk;
+  #repack_ramdisk;
   flash_boot;
   flash_dtbo;
 }
