8db7f8bea9a33748239708c2a926fe0653364818
Danny Lin <danny@kdrag0n.dev>
2020-07-06 18:07:28 -0700

    Revert "arm64:camera:Enable IMX362 Eeprom and vcm"
    This reverts commit 71505280be1caeff37ebda0b15d0a57d60c92ffa.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- include/uapi/media/msm_camsensor_sdk.h
+++ include/uapi/media/msm_camsensor_sdk.h
@@ -46,7 +46,7 @@
 #define MAX_LED_TRIGGERS          3
 
 #define MSM_EEPROM_MEMORY_MAP_MAX_SIZE  80
-#define MSM_EEPROM_MAX_MEM_MAP_CNT      20
+#define MSM_EEPROM_MAX_MEM_MAP_CNT      8
 
 enum msm_sensor_camera_id_t {
 	CAMERA_0,
