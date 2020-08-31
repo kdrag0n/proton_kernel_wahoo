c670e6e05565f5944fd668e78d7ba2a469794d6e
Danny Lin <danny@kdrag0n.dev>
2020-08-31 01:11:59 -0700

    Revert "binder: Fix 4.19 binder compilation on 4.4"
    This reverts commit 04432144982d7b14a0574d630a1feebcb7d23c1a.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -18,7 +18,7 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/list.h>
-#include <linux/mm.h>
+#include <linux/sched/mm.h>
 #include <linux/module.h>
 #include <linux/rtmutex.h>
 #include <linux/rbtree.h>
@@ -723,7 +723,7 @@ int binder_alloc_mmap_handler(struct binder_alloc *alloc,
 	binder_insert_free_buffer(alloc, buffer);
 	alloc->free_async_space = alloc->buffer_size / 2;
 	binder_alloc_set_vma(alloc, vma);
-	atomic_inc(&alloc->vma_vm_mm->mm_count);
+	mmgrab(alloc->vma_vm_mm);
 
 	return 0;
 
@@ -948,7 +948,7 @@ enum lru_status binder_alloc_free_page(struct list_head *item,
 	if (vma) {
 		trace_binder_unmap_user_start(alloc, index);
 
-		zap_page_range(vma, page_addr, PAGE_SIZE, NULL);
+		zap_page_range(vma, page_addr, PAGE_SIZE);
 
 		trace_binder_unmap_user_end(alloc, index);
 	}
